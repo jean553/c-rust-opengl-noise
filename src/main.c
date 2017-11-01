129ba3cc90e0f8c320644eaceb340c9258a337f2
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-01 15:41:52 +0100

    create a simple window with glut

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <GL/glut.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+/**
+ * @brief renders the scene
+ */
+void render() {
+}
+
+/**
+ * @brief program starting function
+ */
 int main(int argc, char* argv[]) {
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+
+    glutInitWindowSize(
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT
+    );
+    glutCreateWindow(NULL);
+
+    glutDisplayFunc(render);
+    glutMainLoop();
 
     return 0;
 }
